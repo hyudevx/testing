8f2b1c198ff4dab4f719266f3929a69b559108b4
Wahyu Diyanto <83213988+hyudevx@users.noreply.github.com>
2023-02-04 21:38:48 +0700

    Add files via upload

---
+++ bypass-dns.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+
+echo ""
+
+ping -c 2 $2 | head -n 3
+
+echo ""
+
+sudo echo $1 " " $2  >> /etc/hosts
+
+echo -e  ">>> Domain $2 resolved to $1 in local \n"
+
+ping -c 2 $2 | head -n 3
+
+echo ""
+
+open -n -a "Brave Browser" --args -incognito "--new-window" http://$2
